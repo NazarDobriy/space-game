2ff5ad56567264dd137b795e3fea4b7a30b58070
Nazar <nazaranddobriy@gmail.com>
2026-08-13 10:38:41 +0300

    remove dist

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-import { Application } from 'pixi.js';
-const app = new Application();
-await app.init({
-    resizeTo: window,
-    backgroundColor: 0x30C790,
-});
-document.body.appendChild(app.canvas);
